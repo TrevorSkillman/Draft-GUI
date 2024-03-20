49d71dfc22400f8bee3c2726ae469f07cada2eb4
Trevor Skillman <trevor.skillman@snhu.edu>
2024-03-20 15:34:50 -0400

    My first initial gui layout for my fantasy draft project

---
+++ main.py
@@ -0,0 +1,65 @@
+from tkinter import *
+from players import *
+
+window = Tk()
+window.title("Draft Practice")
+window.minsize(width=500, height=500)
+window.config(padx=20, pady=20)
+
+# Grid columns https://www.pythontutorial.net/tkinter/tkinter-grid/
+window.columnconfigure(0, weight=1)
+window.columnconfigure(1, weight=1)
+window.columnconfigure(2, weight=1)
+window.columnconfigure(3, weight=1)
+window.columnconfigure(4, weight=1)
+
+# Grid Rows
+window.rowconfigure(0, weight=1)
+window.rowconfigure(1, weight=1)
+window.rowconfigure(2, weight=1)
+window.rowconfigure(3, weight=1)
+window.rowconfigure(4, weight=1)
+
+# Creating label above the list box of players
+player_label = Label(text="Player List")
+player_label.grid(column=0, row=1, sticky="nsew")
+# Creating a label to go above the list box of users drafted players
+user_label = Label(text="Drafted Players")
+user_label.grid(column=4, row=1, sticky="nsew")
+
+# Creating a button to draft the chosen player
+draft_player = Button(window, text="Draft Chosen Player")
+# Using 'columnspan' to ensure it expands across multiple columns to help center the button
+draft_player.grid(column=1, row=2, columnspan=3, sticky="ew")
+
+# Creating Radio buttons for each position being drafted and each position selected
+qb_radio = Radiobutton(text="QB")
+qb_radio.grid(column=0, row=4)
+
+rb_radio = Radiobutton(text="RB")
+rb_radio.grid(column=1, row=4)
+
+wr_radio = Radiobutton(text="WR")
+wr_radio.grid(column=2, row=4)
+
+te_radio = Radiobutton(text="TE")
+te_radio.grid(column=3, row=4)
+
+def_radio = Radiobutton(text="DEF")
+def_radio.grid(column=4, row=4)
+
+# List box
+listbox = Listbox(window, width=10)
+listbox.grid(column=0, row=2, sticky="nsew")
+# The i being the players name and value being the position
+for i, value in players.items():
+    listbox.insert(END, f"{i}: {value}")
+
+# Create another List box for the players that were drafted to the users team
+user_listbox = Listbox(window, width=10)
+user_listbox.grid(column=4, row=2, sticky="nsew")
+# the _ being the flag for the positions to be displayed
+for _ in user_players:
+    user_listbox.insert(END, f"{_}:")
+
+window.mainloop()
---
+++ players.py
@@ -0,0 +1,59 @@
+from tkinter import END
+
+# Just creating a default dictionary with random players
+players = {
+    "QB": [
+        "Daniel Jones",
+        "Dak Prescott",
+        "Jalen Hurts",
+        "Jacoby Brisset"
+    ],
+
+    "RB": [
+        "Devin Singletary",
+        "Saquan Barkley",
+        "Austin Ekler",
+        "Tony Pollard"
+    ],
+
+    "TE": [
+        "Darren Waller",
+        "Jake Ferguson",
+        "Logan Thomas",
+        "Dallas Goedert"
+    ],
+
+    "WR": [
+        "Darius Slayton",
+        "Wandale Moore",
+        "Ceedee Lamb",
+        "Brandin Cooks",
+        "Terry McLaurin",
+        "Jahon Dotson",
+        "AJ Brown",
+        "Devonta Smith"
+    ],
+
+    "DEF": [
+        "Giants",
+        "Cowboys",
+        "Commanders",
+        "Eagles"
+    ]
+}
+# Creating a dictionary for the user team that drafted the players according to the position of the player
+user_players = {
+    "QB": "",
+    "RB": "",
+    "TE": "",
+    "WR": "",
+    "DEF": ""
+}
+
+
+# Creating functionality of a player being drafted to the empty dictionary of the users team
+# def drafted_player(listbox, user_listbox, players, user_players):
+
+
+# Need a function to update both list boxes when a player is chosen to be drafted
+# def update_listboxes(listbox, user_listbox, players, user_players):
